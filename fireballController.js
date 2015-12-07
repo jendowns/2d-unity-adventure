b88a1c560caaaec7c7b89667ee8cc890f050c37f
Jen Downs <hello@jendowns.com>
2015-12-07 00:04:23 -0600

    fireballs are timed, have a spread

--- fireballController.js
+++ fireballController.js
@@ -2,34 +2,51 @@
 
 enum FireballState { Start, Launch, Flying, Hit };
 
-var fireball : Animator;
-var speed : float = 1.0f;
-var launchDurationSecs : float = 1.0f;
+class Boundary
+{
+    var xMin : float;
+    var xMax : float;
+    var yMin : float;
+    var yMax : float;
+}
 
+var boundary : Boundary; // smaller than the entire map!
+var speed : float = 6.0f;
+var launchDurationSecs : float = 0.05f;
+var shotSpreadDeg : float = 30f;
+
+private var fireball : Animator;
 private var state : FireballState = FireballState.Start;
 private var launchTime : float = 0.0f;
 private var enemy : GameObject;
 private var enemyHurt : boolean = false;
 
 function Start () {
-    
+	fireball = GetComponent(Animator);
+	
+	// add an initial randomness to the travel direction
+	transform.Rotate(Vector3(0,0,Random.value*shotSpreadDeg - (shotSpreadDeg/2)));
 }
 
-function OnCollisionEnter(col: Collision){
+function OnTriggerEnter2D(col: Collider2D){
     if(col.gameObject.name == "bahamut"){
         enemy = col.gameObject;
         state = FireballState.Hit;
     }
 }
 
+// destroy the fireball once its hit animation is complete
+function DestroyFireball(){
+	Destroy(this.gameObject);
+}
+
 function Update () {
-    // update velocity (but not position! will only change if we're in flight)
-    
-    var velocity : Vector2 = Vector2.right;
-    velocity = transform.rotation * velocity; // rotate our velocity vector appropriately
-    
-    // update animation
-    
+	// if we've flown past the edge of the battle boundary, or started outside
+	if(transform.position.x < boundary.xMin || boundary.xMax < transform.position.x || transform.position.y < boundary.yMin || boundary.yMax < transform.position.y){
+		DestroyFireball();
+		return;		
+	}
+
     fireball.SetBool("fired",false);
     fireball.SetBool("going",false);
     fireball.SetBool("hit",false);
@@ -46,22 +63,14 @@ function Update () {
             break;
         case FireballState.Flying:
             fireball.SetBool("going",true);
-            transform.position += velocity * speed * Time.deltaTime;
-            
-            // TODO Destroy(this.gameObject) if we've flown past the edge of the game area
-            
+            transform.Translate(Vector3.right * speed * Time.deltaTime); // move the shot relative to its local axes
             break;
         case FireballState.Hit:
             fireball.SetBool("hit",true);
             
-            if(!enemyHurt){
-                //enemy.GetComponent(enemyController).hurt(20); // TODO I made up the hurt() method!
+            if(!enemyHurt){ // a single fireball can only hurt once before dissipating
+                enemy.GetComponent(enemyController).Hurt();
                 enemyHurt = true;
-            }
-            
-            // destroy the fireball once its hit animation is complete
-            if (fireball.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !fireball.IsInTransition(0)){
-                Destroy(this.gameObject);
             }
             break;
     }
